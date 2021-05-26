180c504f486fb32e15255a754cb97e4a2cbd5b45
Leonid <leo-pereper@yandex.ru>
2021-05-26 23:23:59 +0300

    continue on task_10_1

--- Perper_Leonid_dz_10/task_10_1.py
+++ Perper_Leonid_dz_10/task_10_1.py
@@ -8,19 +8,36 @@
 Результатом сложения должна быть новая матрица.
 """
 
+
 class Matrix:
-    def __init__(self, *rows):
-        self.check_matrix(rows)
-        self.__rows = [el for el in rows]
+    def __init__(self, *args):
+        self.check_matrix(*args)
+        self.__rows = [el for el in args]
+
+    def __str__(self):
+        matr_str = []
+        for row in self.__rows:
+            str_row = ' '.join([str(elem) for elem in row])
+            matr_str.append(str_row)
+        return '\n'.join(matr_str)
+
+    def __add__(self, other):
+        if self.get_size() != other.get_size():
+            raise MatrixError('Both summand must have the same size')
+        return Matrix([[elem1 + elem2 for elem1, elem2 in zip(row1, row2)]
+                      for row1, row2 in zip(self.__rows, other.__rows)])
+
+    def get_size(self):
+        return len(self.__rows[0]), len(self.__rows)
 
     @staticmethod
-    def check_matrix(*rows):
+    def check_matrix(*args):
         try:
-            column_num = len(rows[0])
+            column_num = len(args[0])
         except TypeError:
             raise MatrixError('Object can not be interpreted as matrix row')
-        for row in rows:
-            if not isinstance(row, list) and (len(row) == column_num)):
+        for row in args:
+            if not isinstance(row, list) or len(row) != column_num:
                 raise MatrixError('Rows must have equal length')
 
 
@@ -28,4 +45,14 @@ class MatrixError(Exception):
     pass
 
 
-matr_1 = Matrix([1, 2, 3], [4, 5, 6])
+matrix_1 = Matrix([1, 2, 3, 8],
+                  [4, 5, 8, 7],
+                  [5, 8, 7, 7])
+
+matrix_2 = Matrix([1, 2, 3, 8],
+                  [4, 5, 8, 7],
+                  [5, 8, 7, 7])
+matrix_3 = matrix_1 + matrix_2
+print(matrix_3, type(matrix_3), sep='\n')
+
+
