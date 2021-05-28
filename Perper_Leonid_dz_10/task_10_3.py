d94b060e4d1a48edf010dc5b4c500d82c0fd5a40
Leonid <leo-pereper@yandex.ru>
2021-05-28 20:16:24 +0300

    finished task_10_3

--- Perper_Leonid_dz_10/task_10_3.py
+++ Perper_Leonid_dz_10/task_10_3.py
@@ -26,4 +26,61 @@
 
 class Cell:
     def __init__(self, num: int):
-        self.cell_num = num
+        self.elem_num = num
+
+    def __str__(self):
+        return f'Cell of {self.elem_num} elements'
+
+    def __add__(self, other):
+        Cell.check_arg(other)
+        return Cell(self.elem_num + other.elem_num)
+
+    def __sub__(self, other):
+        Cell.check_arg(other)
+        result_cell_num = self.elem_num - other.elem_num
+        if result_cell_num <= 0:
+            raise ValueError(f'Can not subtract {other} form self')
+        return Cell(result_cell_num)
+
+    def __mul__(self, other):
+        Cell.check_arg(other)
+        return Cell(self.elem_num * other.elem_num)
+
+    def __floordiv__(self, other):
+        Cell.check_arg(other)
+        return Cell(self.elem_num // other.elem_num)
+
+    def __truediv__(self, other):
+        return self // other
+
+    def make_order(self, lines_num: int):
+        _lines = [str().ljust(lines_num, '*') for _iter in range(self.elem_num // lines_num)]
+        if self.elem_num % lines_num:
+            _lines.append(str().ljust(self.elem_num % lines_num, '*'))
+        return '\n'.join(_lines)
+
+    @staticmethod
+    def check_arg(item):
+        if not isinstance(item, Cell):
+            raise TypeError('Can not perform operation on not a Cell object')
+
+
+test_item1 = Cell(8)
+test_item2 = Cell(3)
+print(test_item1, type(test_item1))
+print(test_item1.make_order(5))
+# add
+test_result = test_item1 + test_item2
+print(test_result, type(test_result))
+# subtract
+test_result = test_item1 - test_item2
+print(test_result, type(test_result))
+# multiply
+test_result = test_item1 * test_item2
+print(test_result, type(test_result))
+# division
+test_result = test_item1 / test_item2
+print(test_result, type(test_result))
+# integer division
+test_result = test_item1 // test_item2
+print(test_result, type(test_result))
